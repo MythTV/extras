62ffcff5e340dc520718ecc78105649d2a893d10
Chris Petersen <cpetersen@mythtv.org>
2006-07-19 07:49:03 +0000

    Full h.264 ipod encoding.  This requires the SVN (not CVS) version of ffmpeg, but it works wonderfully

--- trunk/export/ffmpeg/iPod.pm
+++ trunk/export/ffmpeg/iPod.pm
@@ -98,19 +98,25 @@ package export::ffmpeg::iPod;
     # Video options
         if (!$is_cli) {
         # Video codec
-            while (1) {
-                my $codec = query_text('Video codec (xvid or h264)?',
-                                       'string',
-                                       $self->{'ipod_codec'});
-                if ($codec =~ /^x/) {
-                    $self->{'ipod_codec'} = 'xvid';
-                    last;
+            if ($self->{'ffmpeg_vers'} eq 'svn') {
+                while (1) {
+                    my $codec = query_text('Video codec (xvid or h264)?',
+                                           'string',
+                                           $self->{'ipod_codec'});
+                    if ($codec =~ /^x/) {
+                        $self->{'ipod_codec'} = 'xvid';
+                        last;
+                    }
+                    elsif ($codec =~ /^h/) {
+                        $self->{'ipod_codec'} = 'h264';
+                        last;
+                    }
+                    print "Please choose either xvid or h264\n";
                 }
-                elsif ($codec =~ /^h/) {
-                    $self->{'ipod_codec'} = 'h264';
-                    last;
-                }
-                print "Please choose either xvid or h264\n";
+            }
+            else {
+                $self->{'ipod_codec'} = 'xvid';
+                print "Using the mpeg4 codec (h.264 ipod encoding requires the svn version of ffmpeg.)\n";
             }
         # Video bitrate options
             $self->{'vbr'} = query_text('Variable bitrate video?',
@@ -145,17 +151,15 @@ package export::ffmpeg::iPod;
                                               'int',
                                               $self->val('v_bitrate'));
         }
+    # Complain about h264
+        if ($self->{'ipod_codec'} eq 'h264' && $self->{'ffmpeg_vers'} ne 'svn') {
+            die "h.264 ipod encoding requires the svn version of ffmpeg.\n";
+        }
     }
 
     sub export {
         my $self    = shift;
         my $episode = shift;
-    # Warn about h264
-        if ($self->{'ipod_codec'} eq 'h264') {
-            print "Please be warned that h264 support is experimental.  I have not yet\n",
-                  "been able to export a working file with h264, and would love any help\n",
-                  "you can offer me in figuring out what's wrong.\n";
-        }
     # Force to 4:3 aspect ratio
         $self->{'out_aspect'}       = 1.3333;
         $self->{'aspect_stretched'} = 1;
@@ -163,9 +167,13 @@ package export::ffmpeg::iPod;
         my $standard = ($episode->{'finfo'}{'fps'} =~ /^2(?:5|4\.9)/) ? 'PAL' : 'NTSC';
         $self->{'width'}   = 320;
         $self->{'height'}  = ($standard eq 'PAL') ? '288' : '240';
-        $self->{'out_fps'} = ($standard eq 'PAL') ? 25    : 29.97;
+        $self->{'out_fps'} = ($standard eq 'PAL') ? 25    : 23.976023976;
     # Embed the title
         my $safe_title = shell_escape($episode->{'show_name'}.' - '.$episode->{'title'});
+    # Build the common ffmpeg string
+        my $ffmpeg_xtra  = ' -vcodec '.$self->{'ipod_codec'}
+                          .' -b '     .$self->{'v_bitrate'}
+                          ." -title $safe_title";
     # Dual pass?
         if ($self->{'multipass'}) {
         # Apparently, the -passlogfile option doesn't work for h264, so we need
@@ -175,13 +183,6 @@ package export::ffmpeg::iPod;
                    ."file, and x264_2pass.log exists in this directory already.  Please\n"
                    ."wait for the other process to finish, or remove the stale file.\n";
             }
-        # Build the common ffmpeg string
-            my $ffmpeg_xtra  = ' -bufsize 65535'
-                              .' -g 300 -acodec aac -async 1'
-                              .' -ab '    .$self->{'a_bitrate'}
-                              .' -vcodec '.$self->{'ipod_codec'}
-                              .' -b '     .$self->{'v_bitrate'}
-                              ." -f mp4 -title $safe_title";
         # Add the temporary files to the list
             push @tmpfiles, 'x264_2pass.log',
                             'x264_2pass.log.temp',
@@ -189,28 +190,72 @@ package export::ffmpeg::iPod;
         # Back up the path and use /dev/null for the first pass
             my $path_bak = $self->{'path'};
             $self->{'path'} = '/dev/null';
+        # A couple of extra options required for h.264
+            if ($self->{'ipod_codec'} eq 'h264') {
+                $ffmpeg_xtra .= ' -vcodec h264'
+                               .' -level 13'
+                               .' -flags +loop -chroma 1'
+                               .' -keyint_min 25 -sc_threshold 40 -i_quant_factor 0.71'
+                               .' -bit_rate_tolerance '.$self->{'v_bitrate'}
+                               .' -rc_max_rate 768000 -rc_buffer_size 2000000'
+                               .' -rc_eq \'blurCplx^(1-qComp)\''
+                               .' -coder 0 -me_range 16 -gop_size 250';
+            }
+            else {
+                $ffmpeg_xtra .= ' -mbd 2 -flags +4mv+trell -aic 2'
+                               .' -cmp 2 -subcmp 2';
+            }
         # Build the ffmpeg string
             print "First pass...\n";
-            $self->{'ffmpeg_xtra'} = ' -pass 1 '.$ffmpeg_xtra;
+            $self->{'ffmpeg_xtra'} = ' -pass 1 '
+                                    .' -acodec copy'
+                                    .$ffmpeg_xtra
+                                    .' -qcompress 0.6 -qmin 10 -qmax 51 -max_qdiff 4'
+                                    .' -f mp4';
+            if ($self->{'ipod_codec'} eq 'h264') {
+                $self->{'ffmpeg_xtra'} .= ' -partitions 0 -flags2 0 -me_method 5'
+                                         .' -subq 1 -trellis 0 -refs 1';
+            }
             $self->SUPER::export($episode, '');
         # Restore the path
             $self->{'path'} = $path_bak;
         # Second Pass
             print "Final pass...\n";
-            $self->{'ffmpeg_xtra'} = ' -pass 2 '.$ffmpeg_xtra;
+            $self->{'ffmpeg_xtra'} = ' -pass 2 '
+                                    .' -acodec aac -ar 48000 -async 1'
+                                    .' -ab '.$self->{'a_bitrate'}
+                                    .$ffmpeg_xtra;
+            if ($self->{'ipod_codec'} eq 'h264') {
+                $self->{'ffmpeg_xtra'} .= ' -partitions partp8x8+partb8x8'
+                                         .' -flags2 +mixed_refs -me_method 8'
+                                         .' -subq 7 -trellis 2 -refs 5';
+            }
         }
     # Single Pass
         else {
-            $self->{'ffmpeg_xtra'}  = ' -bufsize 65535'
-                                     .' -g 300 -acodec aac -async 50'
-                                     .' -ab '    .$self->{'a_bitrate'}
-                                     .' -vcodec '.$self->{'ipod_codec'}
-                                     .' -b '     .$self->{'v_bitrate'}
-                                     .(($self->{'vbr'})
-                                       ? ' -qmin '.$self->{'quantisation'}
-                                        .' -maxrate '.(2*$self->{'v_bitrate'})
-                                       : '')
-                                     ." -f mp4 -title $safe_title";
+            if ($self->{'ipod_codec'} eq 'h264') {
+                $ffmpeg_xtra .= ' -vcodec h264'
+                               .' -flags +loop -chroma 1 -partitions partp8x8+partb8x8'
+                               .' -flags2 +mixed_refs -me_method 8 -subq 7 -trellis 2'
+                               .' -refs 5 -coder 0 -me_range 16 -gop_size 250'
+                               .' -keyint_min 25 -sc_threshold 40 -i_quant_factor 0.71'
+                               .' -bit_rate_tolerance '.$self->{'v_bitrate'}
+                               .' -rc_max_rate 768000 -rc_buffer_size 2000000'
+                               .' -rc_eq \'blurCplx^(1-qComp)\''
+                               .' -level 13';
+            }
+            else {
+                $ffmpeg_xtra .= ' -mbd 2 -flags +4mv+trell -aic 2'
+                               .' -cmp 2 -subcmp 2';
+            }
+            $self->{'ffmpeg_xtra'} = ($self->{'vbr'}
+                                      ? ' -qcompress 0.6 -qmin '.$self->{'quantisation'}
+                                       .' -qmax 51 -max_qdiff 4'
+                                       .' -maxrate '.(2*$self->{'v_bitrate'})
+                                      : '')
+                                    .' -acodec aac -ar 48000 -async 1'
+                                    .' -ab '.$self->{'a_bitrate'}
+                                    .$ffmpeg_xtra;
         }
     # Execute the (final pass) encode
         $self->SUPER::export($episode, '.mp4');
