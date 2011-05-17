06eacd2eda8329e54f8e7c36740f1c0c5c37a70a
Jarod Wilson <jarod@mythtv.org>
2011-05-17 18:14:55 -0400

    Pack mythweb into the release tarball too
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- releasescript/release.sh
+++ releasescript/release.sh
@@ -4,23 +4,39 @@ RELEASEP="0.24.1"
 
 echo "Creating release directory"
 mkdir release > /dev/null
+
 pushd mythtv > /dev/null
 echo "Archiving MythTV v$RELEASEP"
 pushd mythtv > /dev/null
 git archive --format=tar --prefix mythtv-$RELEASEP/ -o ../../release/mythtv-$RELEASEP.tar v$RELEASEP
 popd > /dev/null
+
 pushd mythplugins > /dev/null
 echo "Archiving MythPlugins v$RELEASEP"
 git archive --format=tar --prefix mythplugins-$RELEASEP/ -o ../../release/mythplugins-$RELEASEP.tar v$RELEASEP
 popd > /dev/null
 popd > /dev/null
+
+pushd mythweb > /dev/null
+echo "Archiving MythWeb v$RELEASEP"
+git archive --format=tar --prefix mythplugins-$RELEASEP/mythweb/ -o ../release/mythweb-$RELEASEP.tar v$RELEASEP
+popd > /dev/null
+# We're appending this to the mythplugins tarball, can't use git archive result directly
+pushd release > /dev/null
+tar xf mythweb-$RELEASEP.tar
+tar --append -f mythplugins-$RELEASEP.tar mythplugins-$RELEASEP
+rm -rf mythplugins-$RELEASEP
+popd > /dev/null
+
 pushd release > /dev/null
 echo "Compressing release files"
 bzip2 -9 mythtv-$RELEASEP.tar
 bzip2 -9 mythplugins-$RELEASEP.tar
+
 echo "Computing md5 sums"
 md5sum mythtv-$RELEASEP.tar.bz2      > mythtv-$RELEASEP.md5sum
 md5sum mythplugins-$RELEASEP.tar.bz2 > mythplugins-$RELEASEP.md5sum
+
 echo "Done, created these output files in release directory:"
 ls -lh myth{tv,plugins}-$RELEASEP.{tar.bz2,md5sum}
 popd > /dev/null
